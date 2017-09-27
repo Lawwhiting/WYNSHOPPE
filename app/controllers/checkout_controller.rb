695f1d8b72f08e1db2007c7ac20758344370210b
Lawwhiting <lawwhiting@gmail.com>
2017-09-27 13:34:18 -0400

    added checkout function & page

---
+++ app/controllers/checkout_controller.rb
@@ -0,0 +1,5 @@
+class CheckoutController < ApplicationController
+  def index
+    render 'checkout/index'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
 resources :products, only: [:index]
 resource :cart, only: [:show]
 resources :order_items, only: [:create, :update, :destroy]
+resources :checkout, only: [:index]
+
+  get 'checkout/index'
 
   get 'cart/:id', to: 'carts#show'
 
